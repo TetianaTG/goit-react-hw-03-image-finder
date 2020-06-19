5be92a01b93ed5116c4e3ae7f1dea7b176d331e0
Татьяна <tatana@MacBook-Pro-Tatana.local>
2020-06-19 23:32:12 +0300

    Updates

---
+++ precache-manifest.b5c08a7a45686ecc0b5f8f4d9176afcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f234feaf3cc77fc764afb3b984843731",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5c4f75baccbe13f85bd5",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ed51a4b2787d8dd045f5",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.0cc43360.chunk.css"
+  },
+  {
+    "revision": "5c4f75baccbe13f85bd5",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3269fdd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3269fdd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed51a4b2787d8dd045f5",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.9b362f41.chunk.js"
+  },
+  {
+    "revision": "6c2dba6955eaf9adda16",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.d0b0f768.js"
+  }
+]);
